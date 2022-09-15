54b6021bb5b4d9984bd047a5aba7b5db7da16f3c
Kate Panchugina <e-k-a-t-e-r-i-n-a1@yandex.ru>
2022-09-15 17:44:49 +0300

    max and min number

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Подсказываем, что именно нужно сделать пользователю
+Console.Write("Введите число №1:");
+//ReadLine просит ввести данные, принимает в любом формате, Parse преобразует строку String в int
+int number_a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число №2:");
+int number_b = int.Parse(Console.ReadLine());
+
+if  (number_a>number_b)
+{
+    Console.WriteLine($"минимальное значение = {number_b}, максимальное значение = {number_a}");
+}
+if (number_a<number_b)
+{
+    Console.WriteLine($"минимальное значение = {number_a}, максимальное значение = {number_b}");
+}
+if (number_a==number_b)
+{
+    Console.WriteLine("числа равны");
+}
