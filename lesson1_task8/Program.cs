73844e9ceb53a905c9bc21b7cb53d7bbc9151cef
EkaterinaPanchugina <113240357+EkaterinaPanchugina@users.noreply.github.com>
2022-09-16 20:32:17 +0300

    Урок 1 Задание 8 "Выведение четных чисел от 1 до N

---
+++ lesson1_task8/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+Console.Write("Введите число:");
+//Просим пользователя ввести данные в формате "целое число"
+int number = int.Parse(Console.ReadLine());
+
+int startNumber = 2;
+while(startNumber<=number)
+{
+    Console.WriteLine(startNumber);
+    startNumber = startNumber+2;
+}
