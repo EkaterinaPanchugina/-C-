0c2907a850b37d63526619a6107bf307b5f060f2
EkaterinaPanchugina <113240357+EkaterinaPanchugina@users.noreply.github.com>
2022-09-20 15:08:29 +0300

    Является ли день недели выходным (решение 1)

---
+++ lesson2task15/var1_function/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+void WeekDay (int num)
+
+{
+
+
+if (num == 6) {Console.WriteLine("Да");}
+else if (num == 7) {Console.WriteLine("Да");}
+else if (num > 0 && num < 7) {Console.WriteLine("Нет");}
+else 
+{
+Console.WriteLine("Нет такого дня недели");
+}
+}
+
+Console.Write("Введите номер дня недели:");
+int num = int.Parse(Console.ReadLine());
+
+WeekDay(num);
