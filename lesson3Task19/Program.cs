63a7340ba6e70b03839f40612f7ff47f592e101c
EkaterinaPanchugina <113240357+EkaterinaPanchugina@users.noreply.github.com>
2022-09-27 16:06:46 +0300

    Палиндром пятизначного числа

---
+++ lesson3Task19/Program.cs
@@ -0,0 +1,23 @@
+﻿// Развернуть массив
+Console.Write("Введите число: ");
+    int number = int.Parse (Console.ReadLine());   
+    
+    if (number <=0 )
+    Console.WriteLine("Число не является палиндромом");
+    else
+    {
+    int oldValue = number;
+    int newValue = 0;
+            while (number > 0)
+            {
+                int tmp = number % 10;
+                newValue = newValue * 10 + tmp;
+                number = number / 10;
+            }
+            if (newValue == oldValue)
+                Console.WriteLine("Число является палиндромом");
+            else
+                Console.WriteLine("Число не является палиндромом");
+    }     
+       
+
