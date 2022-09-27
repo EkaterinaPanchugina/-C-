18cc458f26b9c36b96ac522b94a237b0fae71efc
EkaterinaPanchugina <113240357+EkaterinaPanchugina@users.noreply.github.com>
2022-09-27 16:10:27 +0300

    Кубы чисел

---
+++ lesson3Task23/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+int[] GetCubesNumer(int numer)
+{
+    int[] cubesNumers = new int[numer];
+
+    // Заполнение массива результата
+    for (int i = 1; i <= numer; i++)
+    {
+        cubesNumers[i - 1] = i * i * i;
+    }
+
+    return cubesNumers;
+}
+//Запрашиваем у пользователя число и помещаем его в переменную num
+Console.Write("Input name: ");
+int num = int.Parse(Console.ReadLine());
+
+// В качестве результата нам вернется массив из кубов чисел, и передаем метод, который мы сами написали 
+int[] result = GetCubesNumer(num);
+for (int i = 0; i < result.Length; i++) {
+    Console.WriteLine(result[i]);
+}
