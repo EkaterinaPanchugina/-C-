fbc121958af85f84525b87b2e67271d228b6c8b5
EkaterinaPanchugina <113240357+EkaterinaPanchugina@users.noreply.github.com>
2022-09-29 17:48:38 +0300

    массив из 8 элементов

---
+++ lesson4Task29/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Элементы массива вводятся пользователем.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+ int[] GetArray()
+{
+      int[] array = new int[8];
+      for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write("A[{0}] = ", i);
+            array[i] = int.Parse(Console.ReadLine());
+         }
+  return array;
+}
+
+       void PrintArrayToConsole(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+int[] array = GetArray();
+PrintArrayToConsole(array);
