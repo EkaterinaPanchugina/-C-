32015404a221b1fe9e5efb60855328e48a255a80
EkaterinaPanchugina <113240357+EkaterinaPanchugina@users.noreply.github.com>
2022-10-09 20:39:04 +0300

    натуральные числа в промежутке от N до 1

---
+++ lesson9Task64/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет 
+все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+string OutputNumbers(int x)
+{
+    string result = "";
+    if (x == 1)
+    {
+        result = "1";
+    }
+    else
+    {
+          result = $"{x}" + "," +OutputNumbers(x - 1);
+    }
+    return result;
+}
+
+
+
+Console.WriteLine("Введите значение N : ");
+int number = Convert.ToInt32(Console.ReadLine());
+string result = OutputNumbers(number);
+Console.WriteLine($"Промежуток от N {number} до 1 : {result}");
